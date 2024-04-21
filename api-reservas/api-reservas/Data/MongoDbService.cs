ec58fe448247d26a37ab7bcd81369cf98558be71
feuvpi <fredvpg@gmail.com>
2024-04-21 19:55:18 -0300

    web client init

---
+++ api-reservas/api-reservas/Data/MongoDbService.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+
+namespace api_reservas.Data
+{
+    public class MongoDbService
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase? _database;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = configuration.GetConnectionString("DbConnection");
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+    }
+}
